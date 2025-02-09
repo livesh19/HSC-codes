14d92c26d8304f53395a97aa706dd69eaeed39c1
Livesh <117461978+livesh19@users.noreply.github.com>
2025-02-09 08:24:12 +0530

    Add files via upload

---
+++ HSNcode/src/main/java/HSNcode/api/HSNcode/model/HSNDetails.java
@@ -0,0 +1,66 @@
+package HSNcode.api.HSNcode.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class HSNDetails {
+    @Id
+    private String item_code;
+    private String item_name;
+    private String item_type;
+    private int gst_percentage;
+    @Column(name = "hsn_code")
+    private String hsnCode; // Ensure this matches the database column name
+    private String gst_rate;
+
+    // Getters and Setters
+    public String getItem_code() {
+        return item_code;
+    }
+
+    public void setItem_code(String item_code) {
+        this.item_code = item_code;
+    }
+
+    public String getItem_name() {
+        return item_name;
+    }
+
+    public void setItem_name(String item_name) {
+        this.item_name = item_name;
+    }
+
+    public String getItem_type() {
+        return item_type;
+    }
+
+    public void setItem_type(String item_type) {
+        this.item_type = item_type;
+    }
+
+    public int getGst_percentage() {
+        return gst_percentage;
+    }
+
+    public void setGst_percentage(int gst_percentage) {
+        this.gst_percentage = gst_percentage;
+    }
+
+    public String getHsn_code() {
+        return hsnCode;
+    }
+
+    public void setHsn_code(String hsn_code) {
+        this.hsnCode = hsnCode;
+    }
+
+    public String getGst_rate() {
+        return gst_rate;
+    }
+
+    public void setGst_rate(String gst_rate) {
+        this.gst_rate = gst_rate;
+    }
+}
---
+++ HSNcode/src/test/java/HSNcode/api/HSNcode/HsNcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package HSNcode.api.HSNcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HsNcodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
